1da3c6fe4a78bf473ecd6b1e153975b05b87cd9a
Daw <133544883+DawDa07@users.noreply.github.com>
2023-09-02 15:45:42 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,21 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+import folium
 
-df = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
+def main():
+    st.title("Map with Coordinates")
 
-st.map(df)
+    # Input coordinates
+    lat = st.number_input("Enter latitude:", min_value=-90.0, max_value=90.0)
+    lon = st.number_input("Enter longitude:", min_value=-180.0, max_value=180.0)
+
+    # Create a Folium map centered around the input coordinates
+    m = folium.Map(location=[lat, lon], zoom_start=12)
+
+    # Add a marker at the specified coordinates
+    folium.Marker([lat, lon]).add_to(m)
+
+    # Display the map in Streamlit
+    folium_static(m)
+
+if __name__ == "__main__":
+    main()
